bc2165f2d859761dd83e704b38c496e0d362f934
Juan Techera <juanjose@iovlabs.org>
2019-09-05 11:32:18 -0300

    Added Test to ensure network is RSK

---
+++ test/TestIsRSK.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+contract('FirstTest', () => {
+    describe('Verify is RSK', () => {
+      it('Network should be RSK', async () => {
+        let network = await web3.eth.getNodeInfo();
+        assert(network.indexOf('RskJ') >= 0);
+        
+    })
+    })
+  })
