048142eba23eed3dbe48811d2ccee5ee5bbc3639
Juan Techera <juanjose@iovlabs.org>
2019-09-05 12:14:49 -0300

    Added error messages and moved some tests

--- test/TestIsRSK.js
+++ test/TestIsRSK.js
@@ -3,7 +3,7 @@ contract('FirstTest', () => {
     describe('Verify is RSK', () => {
       it('Network should be RSK', async () => {
         let network = await web3.eth.getNodeInfo();
-        assert(network.indexOf('RskJ') >= 0);
+        assert(network.indexOf('RskJ') >= 0,"Network should be RSK but is :" + network);
         
     })
     })
